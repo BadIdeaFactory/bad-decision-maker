17b42015b89af3320ae327441c58ecabc9e4b05c
Chris Zubak-Skees <chriszs@gmail.com>
2020-01-18 18:10:26 -0500

    Add list of poll cards to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
 </svelte:head>
 
 <section>
+  <!--
   <div class="paper-container">
     <a href="/create" rel=prefetch>
       <Paper color="primary" class="paper">
@@ -11,13 +12,56 @@
       </Paper>
     </a>
   </div>
+  -->
+
+  <div class="card-container">
+    <Card>
+      <Content class="mdc-typography--body2">
+        <h2 class="mdc-typography--headline6" style="margin: 0;">Poll title</h2>
+        <!--
+        <h3 class="mdc-typography--subtitle2" style="margin: 0 0 10px; color: #888;">And a subtitle.</h3>
+        It's all in this card. It's a veritable smorgasbord of card features.
+        -->
+      </Content>
+      <a href="/vote" rel=prefetch>
+        <Actions fullBleed>
+          <Button>
+            <Label>Vote</Label>
+            <i class="material-icons" aria-hidden="true">arrow_forward</i>
+          </Button>
+        </Actions>
+      </a>
+    </Card>
+  </div>
+  
+  <div class="card-container">
+    <Card>
+      <Content class="mdc-typography--body2">
+        <h2 class="mdc-typography--headline6" style="margin: 0;">Poll title</h2>
+        <!--
+        <h3 class="mdc-typography--subtitle2" style="margin: 0 0 10px; color: #888;">And a subtitle.</h3>
+        It's all in this card. It's a veritable smorgasbord of card features.
+        -->
+      </Content>
+      <a href="/vote" rel=prefetch>
+        <Actions fullBleed>
+          <Button>
+            <Label>Vote</Label>
+            <i class="material-icons" aria-hidden="true">arrow_forward</i>
+          </Button>
+        </Actions>
+      </a>
+    </Card>
+  </div>
 
   <Fab/>
 </section>
 
 <script>
-  import Paper, {Title, Subtitle, Content} from '@smui/paper';
+  // import Paper, {Title, Subtitle, Content} from '@smui/paper';
   import Fab from '../components/fab.svelte';
+  import Card, {Content, PrimaryAction, Media, MediaContent, Actions, ActionButtons, ActionIcons} from '@smui/card';
+  import Button, { Label } from '@smui/button';
 </script>
 
 <style>
@@ -25,7 +69,7 @@
     text-decoration: none;
   }
 
-  .paper-container {
+  .paper-container, .card-container {
     margin-left: auto;
     margin-right: auto;
     max-width: 600px;
