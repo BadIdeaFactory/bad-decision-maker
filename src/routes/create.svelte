0d6ebfdf796041e0a5154e3af846d1262fe943a8
Chris Zubak-Skees <chriszs@gmail.com>
2020-01-18 17:56:04 -0500

    Adjust create padding

--- src/routes/create.svelte
+++ src/routes/create.svelte
@@ -2,29 +2,31 @@
   <title>Bad Decision Maker</title>
 </svelte:head>
 
-<section class="create-container">
-  <h1>Create</h1>
+<section>
+  <div class="create-container">
+    <h1>Create</h1>
 
-  <div>
-      <div class="field-container">
-        <Textfield fullwidth bind:value={title} label="Title" id="title" />
-        <!-- bind:this={titleEl} -->
-        <!-- input$aria-controls="helper-text-title" input$aria-describedby="helper-text-title"
-        <HelperText id="helper-text-title">Title</HelperText>
-        -->
-      </div>
-      <div class="field-container">
-        <Textfield fullwidth textarea bind:value={description} label="Description" />
-      </div>
-      <div class="field-container">
-        <Textfield fullwidth bind:value={option1} label="Option 1" />
-      </div>
-      <div class="field-container">
-        <Textfield fullwidth bind:value={option1} label="Option 2" />
-      </div>
-      <div class="field-container">
-        <Button variant="raised"><Label>Create</Label></Button>
-      </div>
+    <div>
+        <div class="field-container">
+          <Textfield fullwidth bind:value={title} label="Title" id="title" />
+          <!-- bind:this={titleEl} -->
+          <!-- input$aria-controls="helper-text-title" input$aria-describedby="helper-text-title"
+          <HelperText id="helper-text-title">Title</HelperText>
+          -->
+        </div>
+        <div class="field-container">
+          <Textfield fullwidth textarea bind:value={description} label="Description" />
+        </div>
+        <div class="field-container">
+          <Textfield fullwidth bind:value={option1} label="Option 1" />
+        </div>
+        <div class="field-container">
+          <Textfield fullwidth bind:value={option1} label="Option 2" />
+        </div>
+        <div class="field-container">
+          <Button variant="raised"><Label>Create</Label></Button>
+        </div>
+    </div>
   </div>
 </section>
 
