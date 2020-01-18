a0c7e0d6a514f4c8e5a7b0c0fd23df8c88e0e288
Chris Zubak-Skees <chriszs@gmail.com>
2020-01-18 17:56:20 -0500

    Add vote page mockup

---
+++ src/routes/vote.svelte
@@ -0,0 +1,49 @@
+<svelte:head>
+  <title>Bad Decision Maker</title>
+</svelte:head>
+
+<section>
+  <div class="poll-container">
+    <h1>Poll name</h1>
+
+    <p>Poll description goes here</p>
+
+    <div class="field-container">
+      <Textfield fullwidth bind:value={name} label="Name" />
+    </div>
+
+    <div class="field-container">
+      <Button variant="outlined" style="flex-grow: 1;"><Label>Option 1</Label></Button>
+    </div>
+    <div class="field-container">
+      <Button variant="outlined" style="flex-grow: 1;"><Label>Option 2</Label></Button>
+    </div>
+    <div class="field-container">
+      <Button variant="outlined" style="flex-grow: 1;"><Label>Option 3</Label></Button>
+    </div>
+  </div>
+
+  <Fab/>
+</section>
+
+<script>
+  import Fab from '../components/fab.svelte';
+  import Button, { Label } from '@smui/button';
+  import Textfield, {Input, Textarea} from '@smui/textfield';
+
+  let name = '';
+</script>
+
+<style>
+  .poll-container {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 600px;
+  }
+
+  .field-container {
+    display: flex;
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+</style>
