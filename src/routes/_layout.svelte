640d6ae25411a88cd9047491d146ce442d7916bb
Chris Zubak-Skees <chriszs@gmail.com>
2020-02-29 17:40:34 -0500

    Constrain width again, but wider

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -4,31 +4,33 @@
   callback_url={window.location.origin}
   logout_url={window.location.origin}
 >
-  <TopAppBar variant="standard" class="top-app-bar">
-    <Row>
-      <Section style="padding: 0">
-        <Title component={A} href="/" rel=prefetch class="mdc-theme--primary">
-          Bad Decision Maker
-        </Title>
-      </Section>
-      <Section align="end" toolbar>
-        <IconButton on:click='{() => !$isAuthenticated ? login() : logout() }'>
-          <Icon >
-          {#if !$userInfo || !$userInfo.picture}
-            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-              <path fill="#000000" d="{mdiAccountCircleOutline}" />
-            </svg>
-          {:else}
-            <img alt="user picture" style="width:24px;height:24px;border-radius: 50%;" src="{$userInfo.picture}" />
-          {/if}
-          </Icon>
-        </IconButton>
-      </Section>
-    </Row>
-  </TopAppBar>
-  <main class="main-content">
-    <slot></slot>
-  </main>
+  <div class="app-container">
+    <TopAppBar variant="standard" class="top-app-bar">
+      <Row style="max-width: 850px;">
+        <Section style="padding: 0">
+          <Title component={A} href="/" rel=prefetch class="mdc-theme--primary">
+            Bad Decision Maker
+          </Title>
+        </Section>
+        <Section align="end" toolbar>
+          <IconButton on:click='{() => !$isAuthenticated ? login() : logout() }'>
+            <Icon >
+            {#if !$userInfo || !$userInfo.picture}
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="#000000" d="{mdiAccountCircleOutline}" />
+              </svg>
+            {:else}
+              <img alt="user picture" style="width:24px;height:24px;border-radius: 50%;" src="{$userInfo.picture}" />
+            {/if}
+            </Icon>
+          </IconButton>
+        </Section>
+      </Row>
+    </TopAppBar>
+    <main class="main-content">
+      <slot></slot>
+    </main>
+  </div>
 </Auth0Context>
 
 <script>
@@ -55,6 +57,11 @@
 
 <style>
 .main-content {
-  padding-top: 40px;
+  padding-top: 35px;
+}
+.app-container {
+  max-width: 850px;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
